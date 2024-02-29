6c4928523cb66a58460de856e90eb3eb805daca6
HamzaHajMtir1 <hamzahajmtir2002@gmail.com>
2024-02-29 11:42:31 +0100

    NavBar generate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { CustomersComponent } from './customers/customers.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    CustomersComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
